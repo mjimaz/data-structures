988d9da6f46f2bf925a378992dc1039d7e664cf2
mjimaz <m.aljimaz@gmail.com>
2016-01-13 15:35:38 -0800

    returns size 0 for all size queues

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,17 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue.queueSize = 0;
+  newQueue.storage = {};
+  _.extend(newQueue, queueMethods);
+
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.size = function() {
+  return 0;
+};
 
