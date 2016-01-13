f3b59200568f6e9cf8e54adb1a7aa5ffdef557e7
mjimaz <m.aljimaz@gmail.com>
2016-01-13 15:04:48 -0800

    Implement enque, deque. Finished functional Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,21 @@
 var Queue = function() {
   var someInstance = {};
   var queueSize = 0;
+  var firstItem = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    someInstance[firstItem + queueSize] = value;
     queueSize++;
   };
 
   someInstance.dequeue = function() {
     if(queueSize > 0){
       queueSize--;
+      return someInstance[firstItem++];
     }
   };
 
