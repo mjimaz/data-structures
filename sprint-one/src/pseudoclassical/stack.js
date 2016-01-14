762409edaf0f9a3b209dc236593bee9f45137b06
mjimaz <m.aljimaz@gmail.com>
2016-01-13 16:21:49 -0800

    implement push and pop
    completed pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -2,7 +2,7 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   this.stackSize = 0;
-
+  this.storage = {};
 };
 
 var newStack = new Stack();
@@ -12,9 +12,11 @@ Stack.prototype.size = function() {
 };
 
 Stack.prototype.push = function(value) {
-  this.stackSize++;
+  this.storage[this.stackSize++] = value;
 };
 
 Stack.prototype.pop = function() {
-  this.stackSize--;
+  if (this.stackSize > 0) {
+    return this.storage[--this.stackSize];
+  }
 };
