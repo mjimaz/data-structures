e7974102c21a97fdac01606aba81405fee8b2fd1
mjimaz <m.aljimaz@gmail.com>
2016-01-13 16:24:18 -0800

    return size of zero no matter what!

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,13 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.queueSize = 0;
+  this.storage = {};
+  this.firstItem = 0;
 };
 
+Queue.prototype.size = function(){
+  return this.queueSize;
+};
 
+var newQueue = new Queue();
