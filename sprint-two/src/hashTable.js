36484b412eb78aeaeb207e3a14e66d0d85f74d85
mjimaz <m.aljimaz@gmail.com>
2016-01-15 11:03:32 -0800

    updated inser to avoid collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,12 +7,32 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  
+  //check if item is in hashtable
+  var arrayOfBuckets = this._storage.get(index);
+  
+  //bucket is not empty
+  if(arrayOfBuckets){
+    arrayOfBuckets.push([k,v]);
+  }else{
+    arrayOfBuckets = [[k,v]];
+    this._storage.set(index, arrayOfBuckets);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  var arrayOfBuckets = this._storage.get(index);
+  var value; 
+
+  if(arrayOfBuckets){
+    _.each(arrayOfBuckets, function(bucket){
+      if(bucket[0] === k){
+        value = bucket[1];
+      }
+    });
+  }
+  return value;
 };
 
 HashTable.prototype.remove = function(k) {
