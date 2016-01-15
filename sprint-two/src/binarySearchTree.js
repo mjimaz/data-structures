4cc1844a6ba4b1c243a638036464f45f42d89f9f
mjimaz <m.aljimaz@gmail.com>
2016-01-15 11:44:04 -0800

    added methods to Binary Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,24 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(BinarySearchTree.treeMethods);
+
+  return tree;
 };
 
+
+BinarySearchTree.treeMethods = {};
+
+
+BinarySearchTree.treeMethods.insert = function(value){
+
+};
+
+BinarySearchTree.treeMethods.contains = function(value){
+  return false;
+};
+
+BinarySearchTree.treeMethods.depthFirstLog = function(callback){
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
