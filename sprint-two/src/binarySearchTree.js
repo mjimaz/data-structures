36ca7cde2a3d6f980fabff50d219d86f6f1a4dd1
mjimaz <m.aljimaz@gmail.com>
2016-01-15 11:50:57 -0800

    inserts values at correct locations, and determines if BST contains value

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,10 @@
 var BinarySearchTree = function(value) {
   var tree = Object.create(BinarySearchTree.treeMethods);
 
+  tree.value = value;
+  tree.right = null;
+  tree.left = null;
+
   return tree;
 };
 
@@ -9,11 +13,29 @@ BinarySearchTree.treeMethods = {};
 
 
 BinarySearchTree.treeMethods.insert = function(value){
-
+  if (value > this.value) {
+    if (this.right) {
+      this.right.insert(value);
+    } else {
+      this.right = BinarySearchTree(value);
+    }
+  } else if (value < this.value){
+    if (this.left) {
+      this.left.insert(value);
+    } else {
+      this.left = BinarySearchTree(value);
+    } 
+  }
 };
 
 BinarySearchTree.treeMethods.contains = function(value){
-  return false;
+  if (value === this.value) {
+    return true;
+  } else if (value > this.value) {
+    return !!this.right && this.right.contains(value);
+  } else {
+    return !!this.left && this.left.contains(value);
+  }
 };
 
 BinarySearchTree.treeMethods.depthFirstLog = function(callback){
